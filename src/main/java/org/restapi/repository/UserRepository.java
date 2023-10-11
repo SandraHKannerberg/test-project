3353d11e73018062561d71782d4f8711dfbd6c48
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-10-11 14:13:40 +0200

    basic setup for UserRepository

---
+++ src/main/java/org/restapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.restapi.repository;
+
+import org.restapi.entity.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserRepository {
+
+}
